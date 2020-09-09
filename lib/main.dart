85233afea3ee869fdd550821280863c138baf030
donghwi <pari0130@gmail.com>
2020-09-09 23:02:02 +0900

    2020. 09. 09 test

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sns/config/palette.dart';
+import 'package:sns/screens/screens.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,118 +10,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Today sns app',
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            height: 120,
-            padding: EdgeInsets.only(top: 50, right: 20, left: 20, bottom: 10),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(50),
-                        color: Colors.grey[200]),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(
-                          Icons.search,
-                          color: Colors.grey,
-                        ),
-                        border: InputBorder.none,
-                        hintStyle: TextStyle(color: Colors.grey),
-                        hintText: "Search",
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: 20,
-                ),
-                Icon(
-                  Icons.camera_alt,
-                  color: Colors.grey[800],
-                  size: 30,
-                )
-              ],
-            ),
-          ),
-          Expanded(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Padding(
-                padding: EdgeInsets.all(20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        crossAxisAlignment: CrossAxisAlignment.baseline,
-                        textBaseline: TextBaseline.alphabetic,
-                        children: <Widget>[
-                          Text(
-                            "Stories",
-                            style: TextStyle(
-                                color: Colors.grey[900],
-                                fontWeight: FontWeight.bold,
-                                fontSize: 22,
-                                letterSpacing: 1.2),
-                          ),
-                          Text(
-                              "See Archive"
-                          ),
-                        ],
-                    ),
-                    SizedBox(height:20,),
-                    Container(
-                      height: 180,
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        children: <Widget>[
-                          makeStory()
-                        ],
-                      )
-                    )
-                  ],
-                ),
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget makeStory({storyImage, userImage, userName}){
-    return AspectRatio(
-      aspectRatio: 2 / 2,
-      child: Container(
-        margin: EdgeInsets.only(right: 10),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          image: DecorationImage(
-              image: AssetImage(storyImage),
-              fit: BoxFit.cover
-          )
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          scaffoldBackgroundColor: Palette.scaffold,
         ),
-      ),
-    )
+      home: HomeScreen(),
+    );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:sns/config/palette.dart';
+import 'package:sns/widgets/circle_button.dart';
+import 'package:sns/data/data.dart';
+import 'package:sns/models/models.dart';
+import 'package:sns/widgets/widgets.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: CustomScrollView(slivers: [
+      SliverAppBar(
+        brightness: Brightness.light,
+        backgroundColor: Colors.white,
+        title: Text(
+          'TODAY SNS',
+          style: const TextStyle(
+            color: Palette.facebookBlue,
+            fontSize: 28.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: -1.2, // 상단 title 문구 간격 조정
+          ),
+        ),
+        centerTitle: false,
+        // title 글자 가운데 정렬 여부
+        floating: true,
+        // 스크롤 시 상단 자동 숨김
+        actions: [
+          CircleButton(
+              icon: Icons.search,
+              iconSize: 30.0,
+              onPressed: () => print('Search'),
+          ),
+          CircleButton(
+            icon: MdiIcons.facebookMessenger,
+            iconSize: 30.0,
+            onPressed: () => print('Search'),
+          ),
+        ],
+      )
+    ]));
+  }
+}
